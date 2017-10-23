72fbaf092d79cdae75a573336bb47f0a608cbdbb
Oz Kamara <mackiekbk@yahoo.com>
2017-10-23 10:54:11 -0400

    add a variable, clearInterval and setInterval
    add a variable for the setInterval method. implemented a clearInterval() and a setInterval. also add the random rgbColor() function to my printQuote function.

--- PROJECT 1/js/script.js
+++ PROJECT 1/js/script.js
@@ -1,10 +1,12 @@
-
+let timerOut;
 
 function getRandomQuotes (quotes){
   let randomNumber = Math.floor(Math.random() * quotes.length);
   let getQuotes = quotes[randomNumber];
   return getQuotes;
 }
+window.clearInterval(timerOut);
+timerOut = window.setInterval(printQuote, 10000);
 
 function printQuote(arr) {
 let quoteRandom = getRandomQuotes(quotes);
@@ -12,14 +14,15 @@ let quoteRandom = getRandomQuotes(quotes);
   quoteToPrint += '<p Class="source">' + quoteRandom.source;
   quoteToPrint += '<span class="citation">' + quoteRandom.citation + '</span>';
   quoteToPrint += '<span class="year">' + quoteRandom.year + '</span>' + '</p>';
+  document.body.style.backgroundColor = rgbColor(128, 55, 45);
 return document.querySelector('#quote-box').innerHTML = quoteToPrint;
 }
 
 function rgbColor() {
-let color = 'rgb(';
-color += Math.floor(Math.random () * 256) + ',';
-color += Math.floor(Math.random () * 256) + ',';
-color += Math.floor(Math.random () * 256) + ',';
+  let color = 'rgb(';
+color += Math.floor(Math.random () * 130 + 1) + ',';
+color += Math.floor(Math.random () * 130 + 1) + ',';
+color += Math.floor(Math.random () * 130 + 1);
 color += ')';
 return color;
 }
