5cfb968e2de1f211edad7f0db8f57c3ebffdebb4
Oz Kamara <mackiekbk@yahoo.com>
2017-10-23 12:08:57 -0400

    removed clearInterval(), add the tag categor in quotes.js. from the button a bit lower

--- PROJECT 1/js/script.js
+++ PROJECT 1/js/script.js
@@ -1,11 +1,11 @@
 let timerOut;
 
+
 function getRandomQuotes (quotes){
   let randomNumber = Math.floor(Math.random() * quotes.length);
   let getQuotes = quotes[randomNumber];
   return getQuotes;
 }
-window.clearInterval(timerOut);
 timerOut = window.setInterval(printQuote, 10000);
 
 function printQuote(arr) {
@@ -13,8 +13,10 @@ let quoteRandom = getRandomQuotes(quotes);
   let quoteToPrint = '<p class="quote">' + quoteRandom.quote + '</p>';
   quoteToPrint += '<p Class="source">' + quoteRandom.source;
   quoteToPrint += '<span class="citation">' + quoteRandom.citation + '</span>';
-  quoteToPrint += '<span class="year">' + quoteRandom.year + '</span>' + '</p>';
-  document.body.style.backgroundColor = rgbColor(128, 55, 45);
+  quoteToPrint += '<span class="year">' + quoteRandom.year + '</span>';
+  quoteToPrint += '<span class="tag">, Category: ' + quoteRandom.tag + '</span>';
+  quoteToPrint += '</p>';
+  document.body.style.backgroundColor = rgbColor();
 return document.querySelector('#quote-box').innerHTML = quoteToPrint;
 }
 
