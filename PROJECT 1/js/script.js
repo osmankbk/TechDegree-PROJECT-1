3b9353485b9ddff6b7e4fa5c10e425f06cf742f5
Oz Kamara <mackiekbk@yahoo.com>
2017-10-24 23:04:09 -0400

    Added comments to my project

--- PROJECT 1/js/script.js
+++ PROJECT 1/js/script.js
@@ -1,13 +1,31 @@
+//Title: Random Quote Generator
+
+//The comments below are in order of how i proceeded throug the Project
+
+/*Create the quots array in a different file(quotes.js) to keep things organized
+ * create a function(getRandomQuotes) to retrieve a random quotes from the array
+ *create a function(printQuote) that calls the getRandomQuotes() function and stores the returned quote object in a variable
+ *construct a string in the printQuote() function containing different properties of the quote object and prints out the final html to the page using the innerHTML on the 'quote-box' div
+ *call the printQuote() to display random quotes on the webpage
+ *select the button using querySelector and give an action when click
+ *create a variable (timerOut) that holds the setInterval() methods and give it the printQuote() and 10000 millseconds for parameters
+ *create a function(rgbColor) that returns a random color and added it to the printQuote function
+ *create a variable(usedQuotes) to hold an empty array that stores quotes thats been projected
+ *wrap the printQuote() function in the button's function with a console.log to display quotes projected when click
+ *push the already projected quotes to the empty array(usedQuotes[])
+ *create a splice method that removes a quote from the quotes array each time it returns
+ *add an if statement that checks the quotes[] array if its empty and replennishes it is-
+ - By setting the quotesarray[] equal to the variable holding the usedQuotes(), then usedQuotes back to an empty array[](keeping them in a loop)
+*/
 let timerOut;
 let usedQuotes = [];
 
 
-function getRandomQuotes (quotes){
+function getRandomQuotes (){
   let randomNumber = Math.floor(Math.random() * quotes.length);
   let getQuotes = quotes[randomNumber];
   usedQuotes.push(getQuotes);
   quotes.splice(randomNumber, 1);
-  console.log(quotes.length)
 
   if(quotes.length === 0){
     quotes = usedQuotes;
@@ -17,8 +35,8 @@ function getRandomQuotes (quotes){
 }
 
 timerOut = window.setInterval(printQuote, 10000);
-function printQuote(arr) {
-let quoteRandom = getRandomQuotes(quotes);
+function printQuote() {
+let quoteRandom = getRandomQuotes();
   let quoteToPrint = '<p class="quote">' + quoteRandom.quote + '</p>';
   quoteToPrint += '<p Class="source">' + quoteRandom.source;
   quoteToPrint += '<span class="citation">' + quoteRandom.citation + '</span>';
@@ -38,8 +56,8 @@ color += Math.floor(Math.random () * 130 + 1);
 color += ')';
 return color;
 }
-printQuote(quotes);
+printQuote();
 
 document.querySelector('#loadQuote').addEventListener('click', () => {
-console.log(printQuote(quotes));
+console.log(printQuote());
 })
